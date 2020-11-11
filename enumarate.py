fac27fb0a30de05e5075dc2c1827d2d9fe187724
rams <filius-fall>
2020-11-11 16:11:26 +0530

    enumarate.py

--- enumarate.py
+++ enumarate.py
@@ -1,3 +1,7 @@
 my_list=['apple','banana','orange','grapes','pomogranate']
 
-print(enumerate(my_list))
+#print(enumerate(my_list))  this creates only a object
+
+enum_list=list(enumerate(my_list))
+
+print(enum_list)
